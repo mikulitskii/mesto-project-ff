bad31f6cc7b58fe2cc503b5fdb4a23504985cd94
Nikita Mikulitskii <mikulickiy@yandex.ru>
2024-04-28 12:43:31 +0300

    First Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const cardTemplate = '#card-template';
+const placesList = '.places__list';
 
-// @todo: DOM узлы
+const fillTemplateCard = function (initialCard) {
+    const cardTemplateElement = document.querySelector(cardTemplate).content;
+    const cardElement = cardTemplateElement.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = initialCard.link;
+    cardElement.querySelector('.card__title').textContent = initialCard.name;
+    return cardElement;
+}
 
-// @todo: Функция создания карточки
+const createCard = function (initialCard, deleteCard) {
+    const cardElement = fillTemplateCard(initialCard);
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return cardElement;
+}
 
-// @todo: Функция удаления карточки
+const deleteCard = function (event) {
+    const eventTarget = event.target;
+    eventTarget.parentElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function (initialCard) {
+    const cardElement= createCard(initialCard, deleteCard);
+    document.querySelector(placesList).append(cardElement);
+});
