805e35882228c9375698e564b507bddc69af276e
Nikita Mikulitskii <mikulickiy@yandex.ru>
2024-04-28 18:53:36 +0300

    Fix bugs after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,27 @@
 const cardTemplate = '#card-template';
-const placesList = '.places__list';
+const placesList = document.querySelector('.places__list');
 
 const fillTemplateCard = function (initialCard) {
     const cardTemplateElement = document.querySelector(cardTemplate).content;
     const cardElement = cardTemplateElement.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').src = initialCard.link;
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = initialCard.link;
+    cardImage.alt = `Фотография места ${initialCard.name}`;
     cardElement.querySelector('.card__title').textContent = initialCard.name;
     return cardElement;
 }
 
 const createCard = function (initialCard, deleteCard) {
     const cardElement = fillTemplateCard(initialCard);
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    cardElement.querySelector('.card__delete-button').addEventListener('click',  () => deleteCard(cardElement));
     return cardElement;
 }
 
-const deleteCard = function (event) {
-    const eventTarget = event.target;
-    eventTarget.parentElement.remove();
+const deleteCard = function (card) {
+    card.remove();
 }
 
 initialCards.forEach(function (initialCard) {
     const cardElement= createCard(initialCard, deleteCard);
-    document.querySelector(placesList).append(cardElement);
+    placesList.append(cardElement);
 });
