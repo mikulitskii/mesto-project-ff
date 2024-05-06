94d73e56c1dd9eb608ce0dc150025e2831b63fc5
Nikita Mikulitskii <mikulickiy@yandex.ru>
2024-05-06 18:41:27 +0300

    change name of main js file in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: { main: './src/scripts/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+        filename: 'main.js',
         publicPath: ''
     },
     mode: 'development',
